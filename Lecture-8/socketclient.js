f979c8e2784b144377dd88c72fed3f02e2df7abd
Jasbir <mr.jasbirsingh96@gmail.com>
2020-01-13 18:22:45 +0530

    added socket chat App

--- Lecture-8/socketclient.js
+++
@@ -1,8 +0,0 @@
-// require =>socket -client
-const client = require("socket.io-client");
-// first httop request
-const socket = client.connect("http://localhost:3000");
-
-setTimeout(function() {
-  socket.emit("myevent", "client has just joined");
-}, 2000);
