7ae2a1637bf5fe6ca8604d51c3ffa48308cb340a
Jasbir <mr.jasbirsingh96@gmail.com>
2020-02-16 16:24:02 +0530

    added starting code

---
+++ Lecture-16/canvas.js
@@ -0,0 +1,22 @@
+
+let isMouseDown = false;
+board.addEventListener("mousedown", function (e) {
+
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+    isMouseDown = true
+})
+
+board.addEventListener("mousemove", function (e) {
+    if (isMouseDown == true) {
+        console.log(ctx);
+        ctx.lineTo(e.clientX, e.clientY);
+        
+        ctx.stroke();
+    }
+
+})
+board.addEventListener("mouseup", function (e) {
+    isMouseDown = false;
+    ctx.closePath();
+})
