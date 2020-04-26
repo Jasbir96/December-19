4fbce39f207a521e63639a502683011db931fbf2
Jasbir <mr.jasbirsingh96@gmail.com>
2020-04-26 22:45:04 +0530

    upload image poc done

--- Lecture-20_Backend/poc/ImageUpload/mutliFileUpload.js
+++ Lecture-20_Backend/poc/ImageUpload/mutliFileUpload.js
@@ -23,6 +23,7 @@ const multerStorage = multer.diskStorage({
     cb(null, "public/raw")
   },
   filename: function (req, file, cb) {
+
     cb(null, `user-${Date.now()}.jpeg`)
   }
 })
@@ -32,22 +33,44 @@ const upload = multer({
   storage: multerStorage,
   fileFilter: filter
 })
-
-
-
 app.use(express.json());
-//  mutipart data=> everything=> image  , naming , extension => put 
-app.post("/uploadData", upload.single("photo"), uploadFile)
+//  mutipart data=> everything=> image  , naming , extension => put
+let multiImageHandler = upload.fields([{
+  name: "Cover", maxCount: 1
+}, {
+  name: "images", maxCount: 3
+}]);
+app.post("/uploadData", multiImageHandler, uploadFile)
 async function uploadFile(req, res) {
   try {
-    console.log(req.file);
-    await sharp(req.file.path)
-      .resize(500, 500)
-      .toFormat("jpeg")
-      .jpeg({ quality: 30 })
-      .toFile(`public/final/user-${Date.now()}.jpeg`)
-// // remove file 
-      fs.unlinkSync(req.file.path);
+    // 
+    console.log(req.files);
+    await sharp(req.files.Cover[0].path).resize(2000, 1500).toFormat("jpeg").jpeg({
+      quality: 90
+    }).toFile(`public/final/cover.jpeg`)
+    // cover
+    let promiseArr = [];
+    // start
+    for (let i = 0; i < req.files.images.length; i++) {
+      let filePromise = sharp(req.files.images[i].path)
+        .resize(2000, 1500)
+        .toFormat("jpeg")
+        .jpeg({
+          quality: 90
+        }).toFile(`public/final/image${Date.now()}${i}.jpeg`);
+      promiseArr.push(filePromise); 
+    }
+    await Promise.all(promiseArr);
+
+    console.log("will reach after processing every image");
+    // console.log(req.files.images);
+    // await sharp(req.file.path)
+    //   .resize(500, 500)
+    //   .toFormat("jpeg")
+    //   .jpeg({ quality: 30 })
+    //   .toFile(`public/final/user-${Date.now()}.jpeg`)
+    // // remove file 
+    // fs.unlinkSync(req.file.path);
     res.status(200).json({
       status: "data uploaded successfully"
     })
